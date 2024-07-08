6bffbc401256bdc0a807d361102dd3f97cbdd525
Wilfred <wilfredkim5@gmail.com>
2024-07-08 22:47:59 +0300

    Adding logging, observability and tracing

--- src/main/java/com/wilfred/productservice/productservice/repository/ProductRepository.java
+++ src/main/java/com/wilfred/productservice/productservice/repository/ProductRepository.java
@@ -1,7 +1,8 @@
 package com.wilfred.productservice.productservice.repository;
 
 import com.wilfred.productservice.productservice.model.Product;
+import io.micrometer.observation.annotation.Observed;
 import org.springframework.data.mongodb.repository.MongoRepository;
-
+@Observed
 public interface ProductRepository extends MongoRepository<Product,String> {
 }
